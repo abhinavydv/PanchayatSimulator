0339aac0db0f404f83b3a24d3aac7ab7c9db62ba
AbhinavYdv <abhinavyadavdev@gmail.com>
2023-12-14 14:25:49 +0530

    Added GameBuilder code

--- Assets/Scripts/WorldBuilder.cs
+++ Assets/Scripts/WorldBuilder.cs
@@ -13,9 +13,17 @@ public class WorldBuilder : MonoBehaviour
     }
 
 
-
     void Build(Map map)
     {
-
+        for (int i=0; i<map.objects.Count; i++){
+            MapObject obj = map.objects[i];
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/" + obj.name);
+            // Debug.Log(prefab);
+            // Debug.Log("Prefabs/" + obj.name);
+            GameObject go = Instantiate(prefab, new Vector3(obj.pos.x, prefab.transform.position.y, obj.pos.y), Quaternion.Euler(0, 0, obj.rotation));
+            go.AddComponent<MeshRenderer>();
+            go.transform.localScale = new Vector3(go.transform.localScale.x * obj.size.x, go.transform.localScale.y * obj.size.y, 1);
+            Debug.Log(go.GetComponent<MeshRenderer>().bounds.size);
+        }
     }
 }
