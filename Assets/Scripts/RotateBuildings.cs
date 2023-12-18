93207ab756786a16215bfd0be2c0d99edd10b10e
Vyaas Ramakrishnan <vyaas3305@gmail.com>
2023-12-18 17:16:07 +0530

    Added building rotation

---
+++ Assets/Scripts/RotateBuildings.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotateBuildings : MonoBehaviour
+{
+    private Camera cam = null;
+    public float rotSpeed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateBuilding();
+    }
+
+    void rotateBuilding()
+    {
+        if (Input.GetKey("r"))
+        {
+            // Debug.Log("r down");
+            Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.tag == "Building")
+                {
+                    hit.collider.gameObject.transform.Rotate(0,0,Time.deltaTime*rotSpeed);
+                }
+            }
+        }
+    }
+}
