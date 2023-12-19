e0145e053cbeb7a0bf62017243b2ff812647b485
Antalene_J <119392158+Antalene-J@users.noreply.github.com>
2023-12-19 18:11:16 +0530

    update

--- Assets/Scripts/update_moral.cs
+++ Assets/Scripts/update_moral.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class update_moral : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI scoreText;
     // Update is called once per frame
     void Update()
     {
         int update = SpawnBuildingOnClick.score;
         scoreText.text = update.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Start");
+        }
     }
 }
